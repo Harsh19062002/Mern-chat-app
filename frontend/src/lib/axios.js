ac3ca6e0ec620dfafaacd42579b4719920eea1a0
Harsh19062002 <harshmahajan1962002@gmail.com>
2025-04-24 11:54:38 +0530

    commit

--- frontend/src/lib/axios.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000" : "https://mern-chat-app-t7iz.onrender.com",
-  withCredentials: true,
-});
