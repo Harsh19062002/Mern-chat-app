2fe4127da17a8a2e859d1e56ad6d61f8c2208aa5
Harsh19062002 <harshmahajan1962002@gmail.com>
2025-04-24 11:00:52 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -29,11 +29,11 @@ app.use('/auth',router)
 app.use("/message", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(_dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req,res)=>{
-    res.sendFile(path.join(_dirname,"../frontend","dist","index.html"))
-  })
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
 app.get('/',(req,res)=>{
