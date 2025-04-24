482875bc1dcb32cf2d66467bcaf36f56e91c2765
Harsh19062002 <harshmahajan1962002@gmail.com>
2025-04-24 11:24:42 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -1,32 +1,34 @@
-import express from 'express'
+import express from 'express';
 import dotenv from "dotenv";
-import router from './routes/auth.js'
+import router from './routes/auth.js';
 import { connectDB } from './lib/db.js';
 import cookieParser from "cookie-parser";
 import messageRoutes from "./routes/messageroute.js";
-import cors from 'cors'
-import {app,server} from './lib/socket.js'
-
+import cors from 'cors';
+import { app, server } from './lib/socket.js';
 import path from "path";
+import { fileURLToPath } from 'url';
 
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-dotenv.config()
+dotenv.config();
 
 app.use(express.json({ limit: '50mb' }));
-const PORT = process.env.PORT
-const _dirname = path.resolve();
+const PORT = process.env.PORT;
 
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "http://localhost:5173", 
     credentials: true,
   })
 );
 
-app.use('/auth',router)
+app.use('/auth', router);
 app.use("/message", messageRoutes);
+
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
@@ -36,11 +38,11 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-app.get('/',(req,res)=>{
-    res.send('Backend Is Working')
-})
+app.get('/', (req, res) => {
+  res.send('Backend Is Working');
+});
 
-server.listen(PORT,()=>{
-    console.log('Server is running on Port',PORT)
-    connectDB()
-})
+server.listen(PORT, () => {
+  console.log('Server is running on Port', PORT);
+  connectDB();
+});
